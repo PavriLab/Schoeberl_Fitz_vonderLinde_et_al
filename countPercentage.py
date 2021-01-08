31c2c51092f4a8b50b014422debfd80e79d9f5fa
dmalzl <UaF20091!>
2021-01-08 13:30:40 +0100

    adding new data visualization script

---
+++ countPercentage.py
@@ -0,0 +1,7 @@
+import argparse as ap
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+parser = ap.ArgumentParser()
+parser.add_argument()
+args = parser.parse_args()
