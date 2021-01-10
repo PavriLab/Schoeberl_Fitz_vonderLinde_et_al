37f026e270937457fd69a75ee02b0048cab08545
dmalzl <UaF20091!>
2021-01-10 13:39:30 +0100

    delete new data visualization script because of ipynb generation

--- countPercentage.py
+++
@@ -1,7 +0,0 @@
-import argparse as ap
-import logging
-
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-parser = ap.ArgumentParser()
-parser.add_argument()
-args = parser.parse_args()
